84cab257f34a6506645e093c72a6a67a1c58cb32
deepanshubajaj111 <deepanshu.bajaj111@gmail.com>
2025-03-16 13:13:10 +0530

    footer responsive

--- components/FooterV1.tsx
+++ components/FooterV1.tsx
@@ -1,64 +1,57 @@
 "use client";
 
-import type React from "react";
-
-import { useState } from "react";
-import {
-  Facebook,
-  Instagram,
-  Twitter,
-  ArrowRight,
-  MoveUpRight,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import Image from "next/image";
 import Link from "next/link";
+import type React from "react";
+import Image from "next/image";
+import { useState } from "react";
+import { MoveUpRight } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 export default function FooterV1() {
   const [email, setEmail] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle newsletter subscription
     console.log("Subscribing email:", email);
     setEmail("");
   };
 
   return (
-    <footer className="w-full bg-[#121212] pt-[35px] px-4 pb-[25px]">
+    <footer className="w-full bg-[#121212] pt-[35px] px-4 sm:px-6 md:px-8 lg:px-10 pb-[25px] sm:pb-[32px] md:pb-[40px] lg:pb-[55px] xl:pb-[70px]">
       <div className="max-w-7xl w-full mx-auto space-y-[16px]">
-        <div className="bg-lighter/30 relative py-[18px] px-[22px] overflow-hidden">
-          <div className="absolute -right-5">
+        <div className="bg-lighter/30 relative py-[18px] sm:py-[30px] md:py-[45px] lg:py-[60px] xl:py-[82px] px-[22px] sm:px-[36px] md:px-[50px] lg:px-[65px] xl:px-[77px] overflow-hidden rounded-[6px]">
+          <div className="absolute -right-4 sm:-right-3 w-[155px] sm:w-[180px] md:w-[300px] lg:w-[420px] xl:w-[550px] h-[122px] sm:h-[170px] md:h-[260px] lg:h-[370px] xl:h-[450px] bottom-0">
             <Image
               src="/images/e25d0c33a53a19b554dd3e8a9c8cf7c1.png"
               alt="search-icon"
-              width={172}
-              height={136}
+              fill
+              sizes="100vw"
+              objectFit="cover"
+              objectPosition="top"
             />
           </div>
-          <div className="space-y-4 w-[60%]">
+          <div className="space-y-4 sm:space-y-6 md:space-y-8 lg:space-y-10 w-[60%]">
             <div>
-              <h3 className="text-[24px] leading-[34px] font-black tracking-tight font-saira-condensed upppercase">
+              <h3 className="text-[24px] sm:text-[28px] md:text-[40px] lg:text-[52px] xl:text-[60px] leading-[34px] md:leading-[42px] lg:leading-[58px] xl:leading-[60px] font-black tracking-tight font-saira-condensed upppercase">
                 NEWSLETTER
               </h3>
-              <h3 className="text-[24px] leading-[34px] font-black text-zinc-400 font-saira-condensed upppercase">
+              <h3 className="text-[24px] sm:text-[28px] md:text-[40px] lg:text-[52px] xl:text-[60px] leading-[34px] md:leading-[42px] lg:leading-[58px] xl:leading-[60px] font-black text-zinc-400 font-saira-condensed upppercase">
                 SUBSCRIPTION
               </h3>
             </div>
-            <div>
+            <div className="max-w-[585px]">
               <form onSubmit={handleSubmit} className="flex w-full">
                 <Input
                   type="email"
                   placeholder="aree99@gmail.com"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="bg-[#303030] border-white rounded-s-sm rounded-e-none py-[7.5px] pl-[6px] text-[12px] leading-[16px] h-[31px]"
+                  className="bg-[#303030] border-white rounded-s-sm rounded-e-none py-[7.5px] sm:py-[8px] pl-[6px] sm:pl-[10px] text-[12px] sm:text-[16px] md:text-[18px] lg:text-[21px] leading-[16px] sm:leading-[20px] md:leading-[22px] lg:leading-[28px] h-[31px] sm:h-[38px] md:h-[42px] lg:h-[58px]"
                   required
                 />
                 <button
                   type="submit"
-                  className="bg-white rounded-e-sm hover:bg-zinc-200 w-[31px] h-[31px] flex items-center justify-center"
+                  className="bg-white rounded-e-sm hover:bg-zinc-200 w-[31px] sm:w-[38px] md:w-[42px] lg:w-[85px] h-[31px] sm:h-[38px] md:h-[42px] lg:h-[58px] flex items-center justify-center"
                 >
                   <MoveUpRight className="text-[#84878b] w-4 h-4" />
                 </button>
@@ -69,7 +62,7 @@ export default function FooterV1() {
         <div className="flex items-center space-x-[14px] ">
           <Link
             href="/"
-            className="w-[30px] h-[30px] rounded-sm bg-[#d9d9d936] flex items-center justify-center"
+            className="w-[30px] sm:w-[34px] md:w-[40px] lg:w-[50px] xl:w-[55px] h-[30px] sm:h-[34px] md:h-[40px] lg:h-[50px] xl:h-[55px] rounded-sm bg-[#d9d9d936] flex items-center justify-center"
           >
             <Image
               src="/images/facebook.svg"
@@ -80,7 +73,7 @@ export default function FooterV1() {
           </Link>
           <Link
             href="/"
-            className="w-[30px] h-[30px] rounded-sm bg-[#d9d9d936] flex items-center justify-center"
+            className="w-[30px] sm:w-[34px] md:w-[40px] lg:w-[50px] xl:w-[55px] sm:h-[34px] md:h-[40px] lg:h-[50px] xl:h-[55px] rounded-sm bg-[#d9d9d936] flex items-center justify-center"
           >
             <Image
               src="/images/instagram.svg"
@@ -91,7 +84,7 @@ export default function FooterV1() {
           </Link>
           <Link
             href="/"
-            className="w-[30px] h-[30px] rounded-full bg-[#d9d9d936] flex items-center justify-center"
+            className="w-[30px] sm:w-[34px] md:w-[40px] lg:w-[50px] xl:w-[55px] h-[30px] sm:h-[34px] md:h-[40px] lg:h-[50px] xl:h-[55px] rounded-full bg-[#d9d9d936] flex items-center justify-center"
           >
             <Image
               src="/images/twitter.svg"
@@ -101,51 +94,6 @@ export default function FooterV1() {
             />
           </Link>
         </div>
-        {/* <div className="flex flex-col md:flex-row gap-6 items-center">
-          Newsletter Section
-          <div className="flex-1 w-full space-y-4 relative">
-            <div className="absolute">
-              <Image
-                src="/images/e25d0c33a53a19b554dd3e8a9c8cf7c1.png"
-                alt="footer-image"
-                width={156}
-                height={122}
-              />
-            </div>
-            <div className="space-y-2">
-              <h2 className="text-4xl font-bold tracking-tight">NEWSLETTER</h2>
-              <h3 className="text-3xl font-bold text-zinc-400">SUBSCRIPTION</h3>
-            </div>
-
-            <form
-              onSubmit={handleSubmit}
-              className="flex w-full max-w-md gap-2"
-            >
-              <Input
-                type="email"
-                placeholder="your@email.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="bg-zinc-700 border-zinc-600 text-white placeholder:text-zinc-400"
-                required
-              />
-             
-          </div>
-
-          Image Section
-          <div className="">
-            <Image
-              src="/images/e25d0c33a53a19b554dd3e8a9c8cf7c1.png"
-              alt="Sports Player"
-              width={200}
-              height={200}
-              className="object-cover"
-            />
-          </div>
-        </div>
-
-        Social Media Icons
-         */}
       </div>
     </footer>
   );
